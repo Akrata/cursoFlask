8d54e67af831507c825b2cd4b69b667dd3a210a6
Puagh <pablofeijo181@gmail.com>
2020-04-13 13:55:45 -0300

    template

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
+#crear la ruta
+@app.route('/')
+def index():#esta funcion se encuentra decorada por route
+    name = "codi"
+    return render_template("index.html", username = name)
+
+
+
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
