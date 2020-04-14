7a5efe4172b4f5acf8add89ce71b01a727f79fad
Puagh <pablofeijo181@gmail.com>
2020-04-13 21:23:09 -0300

    finalizando archivos statics

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template,request
 
 app = Flask(__name__)
 
@@ -7,8 +7,22 @@ app = Flask(__name__)
 @app.route('/')
 def index():#esta funcion se encuentra decorada por route
     name = "codi"
-    return render_template("index.html", username = name)
+    course = "Python"
+    is_premium = True
+    courses = ['Python','Java', 'Ruby', 'CSS']
 
+    return render_template("index.html", username = name,course = course, is_premium=is_premium, courses = courses)
+
+@app.route('/usuario/<username>')
+def usuario(username):
+    
+    return "hola " + username
+
+@app.route('/datos')
+def datos():
+    nombre = request.args.get('nombre')
+    curso = request.args.get('curso')
+    return "Listado de datos" + nombre +curso
 
 
 if __name__ == "__main__":
