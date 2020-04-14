e518a20ecfa96666efc344354d60ca7734a547ef
Puagh <pablofeijo181@gmail.com>
2020-04-13 23:32:12 -0300

    finalizando primera parte

--- main.py
+++ main.py
@@ -3,6 +3,18 @@ from flask import render_template,request
 
 app = Flask(__name__)
 
+@app.before_request
+def before_request():
+    print("antes de la peticion")
+
+@app.after_request
+def after_request(response):
+    print ("despues de la peticion")
+    return response
+
+
+
+
 #crear la ruta
 @app.route('/')
 def index():#esta funcion se encuentra decorada por route
@@ -12,6 +24,11 @@ def index():#esta funcion se encuentra decorada por route
     courses = ['Python','Java', 'Ruby', 'CSS']
 
     return render_template("index.html", username = name,course = course, is_premium=is_premium, courses = courses)
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
 
 @app.route('/usuario/<username>')
 def usuario(username):
